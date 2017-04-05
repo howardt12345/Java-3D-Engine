1ee3d4eb3e1e14f1deb3d37e5d27c2b9106263ea
howardt12345 <howardt12345@gmail.com>
2017-04-05 11:28:24 -0400

    Renamed Model to Polyhedron

--- src/Polyhedron.java
+++ src/Polyhedron.java
@@ -3,24 +3,24 @@ import java.util.*;
 import java.awt.*;
 
 @SuppressWarnings("serial")
-/** The Model Class, extends GameObject and implements Serializable.*/
-public class Model extends GameObject implements Serializable {
-	/** The Arraylist of Polygons in the Model.*/
+/** The Polyhedron Class, extends GameObject and implements Serializable.*/
+public class Polyhedron extends GameObject implements Serializable {
+	/** The Arraylist of Polygons in the Polyhedron.*/
 	private ArrayList<Polygon> object = new ArrayList<Polygon>();
 	/** The File name.*/
 	private String FileName;
-	/** Creates a new Model with Transform, Scring filename, and Camera.
-	 * @param transform the Transform of the Model.
+	/** Creates a new Polyhedron with Transform, Scring filename, and Camera.
+	 * @param transform the Transform of the Polyhedron.
 	 * @param filename the file name.
-	 * @param active whether or not the Model is active.
+	 * @param active whether or not the Polyhedron is active.
 	 */
-	public Model (Transform t, String filename, boolean a) {
+	public Polyhedron (Transform t, String filename, boolean a) {
 		super (t);
 		super.setActive(a);
 		ReadFile(filename);
 		FileName = filename;
 	}
-	/** Gets the fileName of the Model.
+	/** Gets the fileName of the Polyhedron.
 	 * @return the fileName
 	 */
 	public String getFileName() {
@@ -53,13 +53,13 @@ public class Model extends GameObject implements Serializable {
 			e.printStackTrace();
 		}
 	}
-	/** The Model transformed by the Model View Perspective Matrix.
-	 * @param model the Model to transform.
+	/** The Polyhedron transformed by the Polyhedron View Perspective Matrix.
+	 * @param polyhedron the Polyhedron to transform.
 	 * @param cam the Camera to transform to.
 	 * @param lights the lights in scene.
 	 */
-	public static Model MVP (Model model, Camera cam, ArrayList<Light> lights) {
-		Model m = (Model) Utils.deepClone(model); //Deep Clones model.
+	public static Polyhedron MVP (Polyhedron polyhedron, Camera cam, ArrayList<Light> lights) {
+		Polyhedron m = (Polyhedron) Utils.deepClone(polyhedron); //Deep Clones model.
 		for (int a = 0; a < m.object.size(); a++) { //Goes through all polygons.
 			m.object.set(a, m.object.get(a).Transform(new Matrix (m.transform.getScale()))); //Scales Polygon.
 			m.object.set(a, m.object.get(a).Transform(new Matrix (m.transform))); //Translates and Rotates Polygon.
@@ -72,7 +72,7 @@ public class Model extends GameObject implements Serializable {
 		}
 		return m;
 	}
-	/** Paints the Model.
+	/** Paints the Polyhedron.
 	 * @param g the Graphics component.
 	 * @param cam the Camera.
 	 * @param lights the lights in scene.
@@ -88,13 +88,13 @@ public class Model extends GameObject implements Serializable {
 			}
 		}
 	}
-	/** Prints the information on the Model.*/
+	/** Prints the information on the Polyhedron.*/
 	public void print () {
-		System.out.println("Model File Name: " + FileName);
+		System.out.println("Polyhedron File Name: " + FileName);
 		System.out.println("Object active: " + Boolean.toString(isActive()));
 		printCoordinates();
 	}
-	/** Prints out the Model Coordinates.*/
+	/** Prints out the Polyhedron Coordinates.*/
 	public void printCoordinates () {
 		for (int a = 0; a < object.size(); a++) {
 			System.out.println("Polygon " + (a+1) + ":");
