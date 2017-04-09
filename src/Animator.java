5f6e229e1167b73e89144e0efd0f1701f49514c9
howardt12345 <howardt12345@gmail.com>
2017-04-09 19:49:01 -0400

    Formatting

--- src/Animator.java
+++ src/Animator.java
@@ -13,7 +13,8 @@ public class Animator {
 	 * @param g the target.
 	 * @param f the JFrame.
 	 */
-	public Animator (Animation a, GameObject g, JFrame f) {
+	public Animator (Animation a, GameObject g, JFrame f) 
+	{
 		anim.add(a);
 		target = g;
 		this.f = f;
@@ -22,20 +23,23 @@ public class Animator {
 	 * @param g the target.
 	 * @param f the JFrame.
 	 */
-	public Animator (GameObject g, JFrame f) {
+	public Animator (GameObject g, JFrame f) 
+	{
 		target = g;
 		this.f = f;
 	}
 	/** Runs all the Animations in the Animator.*/
 	public void play () {
-		for (Animation a : anim) {
+		for (Animation a : anim) 
+		{
 			a.run(target, f);
 		}
 	}
 	/** Adds an Animation to the Animator.
 	 * @param a the Animation to add.
 	 */
-	public void add (Animation a) {
+	public void add (Animation a) 
+	{
 		anim.add(a);
 	}
 }
